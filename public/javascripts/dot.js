1a21c165d333e1fbc7ee1626870be764f7d29ac5
unknown <radianeleven@gmail.com>
2020-12-16 19:27:09 +0200

    re2

--- public/javascripts/dot.js
+++ public/javascripts/dot.js
@@ -1,12 +1,15 @@
 
 $(document).ready(()=>{
-    const url = 'https://ascentromania.herokuapp.com/'
-    const socket = io.connect(url);
-    $(".card .addtocart").click((ev)=>{
-        socket.emit('add-to-cart', currentuser, $(ev.target).parent().children('.name').text())
-
-        console.log((ev.target).parent().children('.name').text(), currentuser, scents)
-        
+    if(currentuser){
+        const url = 'https://ascentromania.herokuapp.com/'
+        const socket = io.connect(url);
+        $(".card .addtocart").click((ev)=>{
+            socket.emit('add-to-cart', currentuser, $(ev.target).parent().parent().children('.name').text())
     
-    })
+            console.log($(ev.target).parent().parent().find('.name').text())
+            
+        
+        })
+    }
+    
 })
