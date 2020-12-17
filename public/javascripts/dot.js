5db16ab14a6ed3ac6a1f679fa8871c45e8176f12
unknown <radianeleven@gmail.com>
2020-12-17 13:08:40 +0200

    re2

--- public/javascripts/dot.js
+++ public/javascripts/dot.js
@@ -6,10 +6,10 @@ $(document).ready(()=>{
         $(".card .addtocart").click((ev)=>{
             console.log('dhw')
             
-            if($(ev.target).text() == 'add_shopping_cart'){
+            if($(ev.target).text().trim() == 'add_shopping_cart'){
                 $(ev.target).text('remove_shopping_cart')
                 socket.emit('add-to-cart', currentuser, $(ev.target).parent().parent().children('.name').text().trim())
-            } else {
+            } else if($(ev.target).text().trim() == 'remove_shopping_cart'){
                 $(ev.target).text('add_shopping_cart')
                 socket.emit('remove-from-cart', currentuser, $(ev.target).parent().parent().children('.name').text().trim())
             }
