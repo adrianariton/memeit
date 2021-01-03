edc9fb23dcc9413830d1da61bf760edbf6de49d2
unknown <radianeleven@gmail.com>
2021-01-03 15:59:53 +0200

    re3

--- app.js
+++ app.js
@@ -51,7 +51,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+if(process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    else
+      next()
+  })
+}
 app.use(express.static(path.join(__dirname, 'public')));
 const mongoc = require('mongodb').MongoClient;
 
