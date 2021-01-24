69259197990e9a23be8c6a526334879b0e49e958
unknown <radianeleven@gmail.com>
2021-01-24 13:03:04 +0200

    re2

--- models/secretcodes.js
+++ models/secretcodes.js
@@ -39,7 +39,8 @@ module.exports.sendSecretUrlToUser = function(uid, email,req, callback){
     console.log({user: process.env.EMAIL_NAME,
       pass: process.env.EMAIL_PASS})
     var transporter = nodemailer.createTransport({
-        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 465,
         secure: true, 
         auth: {
           user: process.env.EMAIL_NAME,
