9a8c389a3616b466dbc41456dba15453d818a2e3
unknown <radianeleven@gmail.com>
2021-02-24 15:53:21 +0200

    re4

--- routes/auth.js
+++ routes/auth.js
@@ -30,7 +30,9 @@ passport.serializeUser((user, done)=>{
 })
 
 passport.deserializeUser((id, done)=>{
+  console.log('\n\n\n\n\n\n ID ')
   console.log(id)
+  console.log('\n\n\n\n\n\n\n\')
   if (id.match(/^[0-9a-fA-F]{24}$/)) {
     User.getUserById(id, (err, user)=>{
       done(err, user)
