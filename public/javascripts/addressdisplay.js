ffcc880d6cd9535a2904a0a0bcfcd89465456171
unknown <radianeleven@gmail.com>
2021-03-15 14:41:59 +0200

    re3

--- public/javascripts/addressdisplay.js
+++ public/javascripts/addressdisplay.js
@@ -5,6 +5,7 @@ $(document).ready(()=>{
   
       $('#addressnr').change(()=>{
         i = $('#addressnr').val()
+        console.log('ddd')
         $('#address-display').text(`${addresses[i].street}, ${addresses[i].city}, ${addresses[i].county}, ${addresses[i].country}; Zip: ${addresses[i].zip}`)
       })
     }
